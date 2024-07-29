8dc391743b3e7fb42475792fe941a3200171100b
equationslayer12 <roy0708usa@gmail.com>
2024-07-29 14:42:43 +0300

    setup socket io

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,16 +3,21 @@ import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Message } from '@/components/ui/message';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import socket from '@/lib/socket';
 
 export function Chat() {
+  useEffect(() => {
+    socket.emit('message', 'Hello');
+  }, []);
+
   const [message, setMessage] = useState();
   const [messages, setMessages] = useState([
     <Message key={0} data='hello world' username='pablo'></Message>,
   ]);
 
   return (
-    <Card className='flex flex-col items-center justify-between w-full max-w-screen-lg h-screen  rounded-lg p-lg bg-slate-600'>
+    <Card className='flex rounded-none flex-col items-center justify-between w-full max-w-screen-lg h-screen p-lg bg-slate-600 md:rounded-lg'>
       <ScrollArea className='w-full'></ScrollArea>
       <Input className='mb-2'></Input>
     </Card>
--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,29 @@
 const PORT = 5000;
 
 const express = require('express');
-const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
-const io = new Server();
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log(message);
+  });
+});
 
 app.get('/', (req, res) => {
   console.log('hello world');
   res.send({ message: 'Hello world!!!' });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server is up and running');
 });
